19b3ba32e5a0a85413ad7402c3d9484f5aa16365
Craig Asher <crasher923@gmail.com>
2017-06-26 12:11:16 -0400

    Added iife

--- part1-review/iife/iife.js
+++ part1-review/iife/iife.js
@@ -1,5 +1,16 @@
 //iife = immediatley invoked function expression
 
+var userAuth = {
+	password : 1234,
+	name : "admin"
+};
+//if any logic preceeds an iife then i need to end line above with ;
+
 (function greeting(){
 	console.log("Heyy you guys!")
-})
+})();
+
+
+(function multiply(){
+	console.log(5 * 4)
+})()
